1305483e962d999c569e4c08292104a1c4193930
hellowoldcrk <ttycracker@gmail.com>
2025-08-16 16:27:01 +0800

    提交

--- static/config.js
+++ static/config.js
@@ -2,4 +2,4 @@ window.BASEPATH="https://api3.orz-orz.cc";
 window.UNIFIED_NUMBER = 100;
 window.Website="https://api3.orz-orz.cc"
 window.WSPATH="wss://api3.orz-orz.cc"
-window.CUSTOMER=true
+window.CUSTOMER=0
